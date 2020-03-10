0acb0f7b84df473b6487262b3672a0f8a8b6df8f
mattbawden <mattbawden@gmail.com>
2020-03-10 11:21:16 +0000

    2 files added

---
+++ src/app-students/app-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-students',
+  templateUrl: './app-students.component.html',
+  styleUrls: ['./app-students.component.css']
+})
+export class AppStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
